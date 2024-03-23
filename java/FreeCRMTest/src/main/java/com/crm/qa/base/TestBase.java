b7376e2e7c017b9263c96c017e46fdd20089c24f
shreyanshraut123 <120080026+shreyanshraut123@users.noreply.github.com>
2024-03-23 10:18:32 +0530

    Add files via upload

---
+++ java/FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,52 @@
+package com.crm.qa.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class TestBase {
+
+	// Global variable
+	public static WebDriver driver;
+	public static Properties prop;
+
+	// Properties read
+	public TestBase() {
+
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(
+					"D:\\java\\FreeCRMTest\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	//The global variable initialized
+	public static void initialization() {
+		 String browserName= prop.getProperty("browser");
+		 
+		 if(browserName.equals("chrome")) {
+			driver =new ChromeDriver();
+		 }else if(browserName.equals("ED")) {
+			 driver = new EdgeDriver();
+		 }
+		 
+		 driver.manage().window().maximize();
+		 driver.manage().deleteAllCookies();
+		 driver.manage().timeouts().pageLoadTimeout( Duration.ofSeconds(com.crm.qa.util.TestUtil.PAGE_LOAD_TIMEOUT ));
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(com.crm.qa.util.TestUtil.IMPLICIT_WAIT));
+		 
+		 driver.get(prop.getProperty("url"));
+	}
+
+}
---
+++ java/FreeCRMTest/src/main/java/com/crm/qa/pages/LoginPage.java
@@ -0,0 +1,54 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+
+
+public class LoginPage extends TestBase {
+	
+	//Page Factory - OR:
+	@FindBy(name="username")
+	WebElement username;
+	
+	@FindBy(name="password")
+	WebElement password;
+	
+	@FindBy(xpath="//input[@type='submit']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath="//a[contains(text(),'Sign Up')]")
+	WebElement signUpBtn;
+	
+	@FindBy(xpath="(//img[contains(@class,'img-responsive')])[1]")
+	WebElement crmLogo;
+	
+	//How we will  uses ini pageFactory
+	//Initializing the Page Object
+	public LoginPage() {
+		PageFactory.initElements(driver, this);//this: current class object 
+	}
+	
+	//Actions:
+	public String validateLoginPageTitle() {
+		return driver.getTitle();
+	}
+	
+	public boolean validatecrmImage() {
+		return crmLogo.isDisplayed();
+	}
+	
+	public HomePage login(String un, String pwd) {
+		username.sendKeys(un);
+		password.sendKeys(pwd);
+		loginBtn.click();
+		
+		return new HomePage();
+		
+	}
+	
+
+}
